e4a9697a9b4a678c94cd1b4f29b38843e04fb72e
JoFz <43348746+FzJo@users.noreply.github.com>
2020-03-03 14:25:13 +0100

    Add Class and use setState()

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
-const ContactForm = () =>
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+class ContactForm extends Component {
+  state = {
+    email: "",
+    message: ""
+  };
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-;
+  handleEmailChange = event => {
+    const email = event.target.value;
+    this.setState({ email });
+    console.log(email);
+  };
+
+  handleMessageChange = event => {
+    const message = event.target.value;
+    this.setState({ message });
+    console.log(message);
+  };
+
+  render = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={this.handleEmailChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Your Messsage</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            placeholder="Enter your message here"
+            onChange={this.handleMessageChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  };
+}
 
 export default ContactForm;

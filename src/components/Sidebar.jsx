1e84ce75e60f4345e9c3d6c1f23ad181fe082829
JoFz <43348746+FzJo@users.noreply.github.com>
2020-03-03 12:26:00 +0100

    Add sidebar links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
-import { articles, categories } from "../data/index";
+import { articles, categories } from "../data";
+import { Link } from "react-router-dom";
 
 const SidebarSection = props => (
   <section>
     <h2>{props.title}</h2>
     <ListGroup>
       {props.items.map((item, index) => (
-        <ListGroup.Item key={`link${index}`}>{item}</ListGroup.Item>
+        <ListGroup.Item key={`link${index}`}>
+          <Link to={`/${props.type}/${index}`}>{item}</Link>
+        </ListGroup.Item>
       ))}
     </ListGroup>
   </section>
 );
-
 const Sidebar = () => (
   <aside>
-    <SidebarSection title="Articles" items={articles.map(item => item.title)} />
     <SidebarSection
-      title="Categories"
+      title="Articles"
+      type="article"
+      items={articles.map(item => item.title)}
+    />
+    <SidebarSection
+      title="Catégories"
+      type="category"
       items={categories.map(item => item.name)}
     />
   </aside>
